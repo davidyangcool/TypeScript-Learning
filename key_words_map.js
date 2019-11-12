aaf18a350516d65b7db474b2c410a17277aa5366
davidyancool <davidyang_cool@icloud.com>
2019-11-12 19:39:42 +0800

    map关键词学习

---
+++ key_words_map.js
@@ -0,0 +1,27 @@
+// const colors =['red','green','blue'];
+
+// const items = colors.map(color=>{
+//     return '<li>'+color+'</li>'
+// })
+
+// console.log(items)
+// console.log(colors)
+
+
+const array =[1,3,6,9];
+
+// const newArray =array.map(function(value){return value+1})
+
+// console.log(newArray)
+
+// const newArray2 =[];
+// for(var i in array){
+//     newArray2.push(array[i]+1)
+// }
+// console.log(newArray2)
+
+
+
+
+ console.log(array)
+
--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-console.log(typeof(Date.now()));
+// console.log(typeof(Date.now()));
+
+// const appVersion = 'v 1.2.3'
+// const result1 = appVersion.split(" ")
+// const prefix = result1[0]
+// const result2 = result1[1].split(".")
+// const first = result2[0]
+// const second = result2[1]
+// const third =result2[2]
+
+// console.log('prefix',prefix)
+// console.log('first',first)
+// console.log('second',second)
+// console.log('third',third)
